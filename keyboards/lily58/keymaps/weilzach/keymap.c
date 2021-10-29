1f7ce99f9a1b1ae99558f0dad93b5f8011fc240e
Zachary Weil <weilzach@amazon.com>
2021-10-28 22:03:03 -0400

    Move enter and make double space

--- keyboards/lily58/keymaps/weilzach/keymap.c
+++ keyboards/lily58/keymaps/weilzach/keymap.c
@@ -15,11 +15,13 @@
 
 #include QMK_KEYBOARD_H
 
+//TODO remove this crap
 #define SY_PLS LT(_SYMBOL, KC_PPLS)
 #define SY_MIN LT(_SYMBOL, KC_MINS)
 #define SY_SPC LT(_SYMBOL, KC_SPC)
 #define G_COMM LGUI_T(KC_COMM)
 #define A_DOT  LALT_T(KC_DOT)
+
 #define ADJ    MO(_ADJUST)
 #define NAV    MO(_NAV)
 
@@ -35,7 +37,6 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     //TODO match keycaps
-    //TODO add basic arrows
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -54,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   SY_MIN,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    SY_PLS,
   KC_GRV,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_ESC,        SY_MIN,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                             ADJ,     KC_LALT, NAV, KC_SFTENT,     SY_SPC,  A_DOT,   KC_HOME, KC_END
+                             ADJ,     KC_LALT, NAV, SY_SPC,     SY_SPC,  A_DOT,   KC_HOME, KC_END
 ),
 
 /* Nav
