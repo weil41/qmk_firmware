5c2fdb403877a094ae764c9de03e3ff68d48fe4a
Zachary Weil <weilzach@amazon.com>
2021-11-03 10:51:30 -0400

    Updated lily58 with * added symbols * Improved symbols on thumbs

--- keyboards/lily58/keymaps/weilzach/keymap.c
+++ keyboards/lily58/keymaps/weilzach/keymap.c
@@ -24,6 +24,7 @@
 
 #define ADJ    MO(_ADJUST)
 #define NAV    MO(_NAV)
+#define SYMBOL MO(_SYMBOL)
 
 //TODO
 
@@ -61,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   SY_MIN,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    SY_PLS,
   KC_GRV,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_ESC,        SY_MIN,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                             KC_LCTRL,     KC_LGUI,  SY_SPC, KC_LGUI,      SY_SPC,  A_DOT,   KC_HOME, KC_END
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_ESC,        KC_QUOT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                             KC_LCTRL,     KC_LGUI,  SY_SPC, KC_MINS,      KC_QUOT,  LT(SYMBOL, KC_SPC),   KC_LGUI, KC_LALT
 ),
 
 /* Nav
@@ -105,9 +106,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_SYMBOL] = LAYOUT(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                   KC_AMPR, KC_PPLS, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                    KC_PEQL, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-  _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______, _______, KC_ASTR, KC_MINS, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,                   KC_AMPR, KC_7, KC_8,   KC_9, XXXXXXX, XXXXXXX,
+  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                    KC_0, KC_4, KC_5, KC_6, XXXXXXX, XXXXXXX,
+  _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______, _______, KC_ASTR, KC_1, KC_2, KC_3, XXXXXXX, _______,
                              _______, _______, _______, _______, _______,  _______, XXXXXXX, XXXXXXX
 ),
 /* ADJUST
